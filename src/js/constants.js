2cb433a5ad12e5c4f150e5fb5c872d8a36e8a4ab
Usman <mizmaar3@hotmail.com>
2016-08-22 15:38:45 +0200

    endpoint change

--- src/js/constants.js
+++ src/js/constants.js
@@ -1,5 +1,6 @@
 let Constants = {
-  dataEndPoint: "https://rawgit.com/markpasternak/52711ab2db70c275dc0690006661dc3a/raw/033d733359443c6875f5b36afc2a371d3222d766/data.json",
+  /*dataEndPoint: "https://rawgit.com/markpasternak/52711ab2db70c275dc0690006661dc3a/raw/033d733359443c6875f5b36afc2a371d3222d766/data.json",*/
+  dataEndPoint: "https://cdn.rawgit.com/mizmaar3/DMVT/master/raw-data.json",
   dataSetName: "campaigns",
   pageTitle: "The Delivery Metric Visualization Tool"
 };
